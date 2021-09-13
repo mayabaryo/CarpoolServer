5a9fad46dd37292566bd855df903462131f54312
User <User@LAB3-19>
2021-09-13 14:33:17 +0300

    Add project files.

---
+++ CarpoolServer/Controllers/CarpoolController.cs
@@ -0,0 +1,23 @@
+﻿using CarpoolServerBL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarpoolServer.Controllers
+{
+    [Route("CarpoolAPI")]
+    [ApiController]
+    public class CarpoolController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        CarpoolDBContext context;
+        public CarpoolController(CarpoolDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
---
+++ CarpoolServerBL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CarpoolServerBL
+{
+    public class Class1
+    {
+    }
+}
